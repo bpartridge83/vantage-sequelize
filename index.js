81df08bfdaca4d8e0c48739b08dbb7dab0864ded
Brian Partridge <bpartridge83@gmail.com>
2015-07-28 15:30:53 -0700

    Trying spread

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 module.exports = function (vantage, options) {
 
-  vantage
-    .mode('sql')
-    .delimiter('sql:')
-    .init(function (args, callback){
-      this.log('Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`.');
-      callback();
-    })
-    .action(function (command, callback) {
+	var _ = require('underscore');
 
-      var self = this;
+	vantage
+		.mode('sql')
+		.delimiter('sql:')
+		.init(function (args, callback){
+			this.log('Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`.');
+			callback();
+		})
+		.action(function (command, callback) {
 
-      pg.query(command).then(function (response) {
-      	console.log('response');
-      	console.log(response);
-      	self.log(response);
-      }).catch(function (error) {
-      	console.error(error);
-      	self.error(response);
-      });
+			var self = this;
 
-    });
+			pg.query(command).spread(function (response, meta) {
+				self.log(response);
+			});
+
+		});
 
 }
