b904f1a18dc268fe429b8002bd1740ea3fabe0b4
Brian Partridge <bpartridge83@gmail.com>
2015-07-28 15:25:41 -0700

    Trying another global

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function (vantage, options) {
 
       var self = this;
 
-      sequelize.query(command).then(function (response) {
+      pg.query(command).then(function (response) {
       	console.log('response');
       	console.log(response);
       	self.log(response);
